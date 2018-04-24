483ee2d3a1158cf90bf9728e0d9788ae24f141f2
developerxiaofeng <31647935+developerxiaofeng@users.noreply.github.com>
2018-04-24 08:46:26 +0800

    201804240845commit
    commit

---
+++ src/main/java/com/prostate/system/service/CityService.java
@@ -0,0 +1,18 @@
+package com.prostate.system.service;
+
+import com.prostate.system.entity.City;
+
+import java.util.List;
+
+/**
+ * @Author: developerfengrui
+ * @Description:
+ * @Date: Created in 14:26 2018/4/23
+ */
+public interface CityService extends BaseService<City>{
+
+    public List<City> findByPage(Integer pageNum,Integer pageSize,String parentCityId);
+
+
+    List<City> selectByCityName(String param);
+}
