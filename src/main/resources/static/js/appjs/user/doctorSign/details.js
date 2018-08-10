59c2df553c07583279a2aa24dca7c9feb09281ca
bian1234 <ykbian@qq.com>
2018-08-10 20:12:42 +0800

    医生认证文件补充
    补充js文件。

---
+++ src/main/resources/static/js/appjs/user/doctorSign/details.js
@@ -0,0 +1,103 @@
+$().ready(function() {
+    validateRule();
+});
+//==========================================这个函数会对提交的表单进行拦截。
+// $.validator.setDefaults({
+//     submitHandler : function() {
+//         update();
+//     }
+// });
+
+//同意认证申请
+function pass() {
+    $.ajax({
+        cache : true,
+        type : "POST",
+        url : "/user/doctorSign/pass",
+        data : $('#signupForm').serialize(),// 你的formid
+        async : false,
+        error : function(request) {
+            parent.layer.alert("Connection error");
+        },
+        success : function(data) {
+            if (data.code == 20000) {
+                parent.layer.msg("认证成功");
+                parent.reLoad();
+                var index = parent.layer.getFrameIndex(window.name); // 获取窗口索引
+                parent.layer.close(index);
+            } else {
+                parent.layer.alert(data.msg)
+            }
+
+        }
+    });
+}
+
+//拒绝认证申请
+function refuse() {
+    $.ajax({
+        cache : true,
+        type : "POST",
+        url : "/user/doctorSign/refuse",
+        data : $('#signupForm').serialize(),// 你的formid
+        async : false,
+        error : function(request) {
+            parent.layer.alert("Connection error");
+        },
+        success : function(data) {
+            if (data.code == 20000) {
+                parent.layer.msg("操作成功");
+                parent.reLoad();
+                var index = parent.layer.getFrameIndex(window.name); // 获取窗口索引
+                parent.layer.close(index);
+
+            } else {
+                parent.layer.alert(data.msg)
+            }
+
+        }
+    });
+}
+
+
+
+function update() {
+    $.ajax({
+        cache : true,
+        type : "POST",
+        url : "/user/doctorSign/update",
+        data : $('#signupForm').serialize(),// 你的formid
+        async : false,
+        error : function(request) {
+            parent.layer.alert("Connection error");
+        },
+        success : function(data) {
+            if (data.code == 20000) {
+                parent.layer.msg("操作成功");
+                parent.reLoad();
+                var index = parent.layer.getFrameIndex(window.name); // 获取窗口索引
+                parent.layer.close(index);
+
+            } else {
+                parent.layer.alert(data.msg)
+            }
+
+        }
+    });
+
+}
+function validateRule() {
+    var icon = "<i class='fa fa-times-circle'></i> ";
+    $("#signupForm").validate({
+        rules : {
+            name : {
+                required : true
+            }
+        },
+        messages : {
+            name : {
+                required : icon + "请输入名字"
+            }
+        }
+    })
+}
